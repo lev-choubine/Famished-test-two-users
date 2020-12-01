6510d3b920dcdb3e2003f6828b8d8776ecf51326
Lev Michael <lev.choubine@gmail.com>
2020-12-01 16:20:10 -0500

    setting up async await for db find All

--- dbTest.js
+++ dbTest.js
@@ -6,7 +6,54 @@ const type = 'Taco';
 const price = 5;
 
 
-
+app.get('/profile/finds',(req,res)=>{
+    db.user_profile.findOne({
+      where: {user_id : req.user.id}
+    })
+    .then(
+      prof => {
+        let street = JSON.stringify(prof.street)
+        let city = JSON.stringify(prof.city)
+        let state = JSON.stringify(prof.state)
+        let zip = JSON.stringify(prof.zip)
+        
+  ////////////////////////////////////////////////////////////////////////
+  db.items.findAll().then(finds => {
+   let find = finds
+   db.user_wants.findAll({
+    where: {user_id : req.user.id}
+  }).then(picks=>{
+   
+    let pick = picks
+    ////////////////////////////////////////////////
+    db.seller_has.findAll({
+      where: {type:'Taco', price:{[Op.lte] : 5}}
+  })
+  .then(finds => {
+  let find = finds
+  console.log('!!!!!!!!!!'+JSON.stringify(picks))
+  res.render('finds', {pass: req.user.name, street, city, state, zip, find, pick, find})
+  }
+  
+  )
+  .catch(console.error)
+  ///////////////////////////////////////////////////////////////
+   
+  })
+  }
+  /////////////////////////////////////////////////////////////////////////
+  )
+  ///////////////////////////////////////////////////////////////////////
+  
+  /////////////////////////////////////////////////////////////////////////
+  
+    
+  })
+    .catch(err=>{console.log(err)})
+    
+    
+    
+  })
 
   //!!!!LESS THAN -   where: {price:{[Op.lte] : 9}},!!!!!
 
--- server.js
+++ server.js
@@ -128,50 +128,71 @@ db.items.findAll().then(finds => {
   
 });
 
-app.get('/profile/finds',(req,res)=>{
-  db.user_profile.findOne({
+app.get('/profile/finds', async (req,res) => {
+  const picks = await db.user_wants.findAll({
     where: {user_id : req.user.id}
   })
-  .then(
-    prof => {
-      let street = JSON.stringify(prof.street)
-      let city = JSON.stringify(prof.city)
-      let state = JSON.stringify(prof.state)
-      let zip = JSON.stringify(prof.zip)
-      
-////////////////////////////////////////////////////////////////////////
-db.items.findAll().then(finds => {
- let find = finds
- db.user_wants.findAll({
-  where: {user_id : req.user.id}
-}).then(picks=>{
- 
-  let pick = picks
-  ////////////////////////////////////////////////
-  db.seller_has.findAll({
-    where: {type:'Taco', price:{[Op.lte] : 5}}
-})
-.then(finds => {
-let find = finds
-console.log('!!!!!!!!!!'+JSON.stringify(picks))
-res.render('finds', {pass: req.user.name, street, city, state, zip, find, pick, find})
-}
+  let found = []
+  picks.forEach( async (p) =>{
+    const find = await  db.seller_has.findAll({
+      where: {type: p.type, price:{[Op.lte]:p.price}}
+    })
+    found=found.concat(find)    
+  })
+ console.log('@@@@@@@@@@@@@'+found)
 
-)
-.catch(console.error)
-///////////////////////////////////////////////////////////////
- 
-})
-}
-/////////////////////////////////////////////////////////////////////////
-)
-///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////  
+//   db.user_profile.findOne({
+//     where: {user_id : req.user.id}
+//   })
+//   //////////1st THEN///////////////////////
+//   .then(
+//     prof => {
+//       let street = JSON.stringify(prof.street)
+//       let city = JSON.stringify(prof.city)
+//       let state = JSON.stringify(prof.state)
+//       let zip = JSON.stringify(prof.zip)
+//       db.items.findAll().then(finds => {
+//  let find = finds
+//  db.user_wants.findAll({
+//   where: {user_id : req.user.id}
+//   /////////////2nd NESTED THEN////////////////
+// }).then(picks=>{
+//   let found = []
+//   let test = 0
+//   let pick = picks
+//   pick.forEach(p => {
+//     db.seller_has.findAll({
+//       where: {type:p.type, price:{[Op.lte] : p.price}}
+//   })
+//   ////////////3rd NESTED THEN//////////////////
+//   .then(finds => {
+//   console.log(JSON.stringify(found))
+//   found.push(finds)
+//   test = test +1
+//   console.log('A'+test)
+//   }
+//   /////////////3rd NESTED THEN
+//   )
+//   console.log('B'+test)
 
-/////////////////////////////////////////////////////////////////////////
-
+//   });
   
-})
-  .catch(err=>{console.log(err)})
+//   console.log('@@@@@@@@'+found);
+  
+  
+// res.render('finds', {pass: req.user.name, street, city, state, zip, find, pick, found})
+// /////////////2nd NESTED THEN//////////////
+// })
+// ////////////////////////////////////
+// }
+// /////////////////////////////////////////////////////////////////////////
+// )
+// ///////////////////////////////////////////////////////////////////////
+
+// /////////////////////////////////1ST THEN////////////////
+// })
+//   .catch(err=>{console.log(err)})
   
   
   
