73924b26a509323629e7fd595dd2d3614d251ab4
Lev Michael <lev.choubine@gmail.com>
2020-12-02 12:29:56 -0500

    both edit methods enabled

--- routes/address.js
+++ routes/address.js
@@ -2,22 +2,19 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
-router.post('/profile', function(req, res) {
+router.put('/profile', function(req, res) {
     console.log('attemting to post');
-    db.user_profile.findOrCreate({
-      where: {
+    db.user_profile.findOne({
+      where: { user_id :req.user.id} 
+    }).then((user  =>{
+      user.update({
         user_id: req.user.id,
         name: req.user.name,
         street: req.body.street,
         city: req.body.city,
         state: req.body.state,
         zip: req.body.zip
-
-       
-        
-      } 
-    }).then((user  =>{
-      console.log(`Storing your address into the database!!`)
+      })
       res.redirect('/profile')
     }))
     
--- routes/seller_address.js
+++ routes/seller_address.js
@@ -2,29 +2,33 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
-router.post('/profile', function(req, res) {
+router.put('/profile', function(req, res) {
     console.log('attemting to post');
-    db.seller_profile.findOrCreate({
-      where: {
-        seller_id: req.user.id,
-        business_name: req.body.business,
-        description: req.body.info,
-        image: req.body.image,
-        street: req.body.street,
-        city: req.body.city,
-        state: req.body.state,
-        zip: req.body.zip,
-        open_at: req.body.open,
-        closes_at:req.body.close
+    db.seller_profile.findOne({
+      where: { seller_id : req.user.id}
+    //    {
+  
         
-      } 
+    //   } 
     }).then((user  =>{
+        user.update ({
+            seller_id: req.user.id,
+            business_name: req.body.business,
+            description: req.body.info,
+            image: req.body.image,
+            street: req.body.street,
+            city: req.body.city,
+            state: req.body.state,
+            zip: req.body.zip,
+            open_at: req.body.open,
+            closes_at:req.body.close 
+        })
       console.log(`Storing your address into the database!!`)
       res.redirect('/profile')
     }))
     
   });
-
+////////////////////////////////////////////////////////////////////
   router.post('/seller_address', function(req, res) {
     console.log('attemting to post');
     db.seller_profile.findOrCreate({
