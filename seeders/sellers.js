45f137a2b223b63e35c5dbaa6d32875dbe26d53c
Lev Michael <lev.choubine@gmail.com>
2020-12-03 16:03:05 -0500

    added a dtabase for heroku

--- seeders/sellers.js
+++ seeders/sellers.js
@@ -1,20 +1,20 @@
-'use strict'
-module.exports = {
-    up: function (queryInterface, Sequelize) {
-      return queryInterface.bulkInsert('sellers', [
-        { name: 'Bob Miller',
-          email: 'truck@gmail.com',
-          password: '$2b$12$tgAh9RynVogA0m.0c1rD..CgGIlrRqMwx8kxjNrCwKOqH0hkaiRUe',
-          type: 'seller',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
+// 'use strict'
+// module.exports = {
+//     up: function (queryInterface, Sequelize) {
+//       return queryInterface.bulkInsert('sellers', [
+//         { name: 'Bob Miller',
+//           email: 'truck@gmail.com',
+//           password: '$2b$12$tgAh9RynVogA0m.0c1rD..CgGIlrRqMwx8kxjNrCwKOqH0hkaiRUe',
+//           type: 'seller',
+//           createdAt: new Date(),
+//           updatedAt: new Date()
+//         },
   
      
-      ], { returning: true })
-    },
+//       ], { returning: true })
+//     },
   
-    down: function (queryInterface, Sequelize) {
-      return queryInterface.bulkDelete('sellers', null, {})
-    }
-  }
+//     down: function (queryInterface, Sequelize) {
+//       return queryInterface.bulkDelete('sellers', null, {})
+//     }
+//   }
--- seeders/users.js
+++ seeders/users.js
@@ -1,19 +1,19 @@
-'use strict'
-module.exports = {
-    up: function (queryInterface, Sequelize) {
-      return queryInterface.bulkInsert('users', [
-        { name: 'Lev Choubine',
-          email: 'lev.choubine@gmail.com',
-          password: '$2b$12$3HfeHDFs.MY1P1oBMwo30.jkW3ZB30.NZ.YTFYghgmO5Xrxj.geeC',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
+// 'use strict'
+// module.exports = {
+//     up: function (queryInterface, Sequelize) {
+//       return queryInterface.bulkInsert('users', [
+//         { name: 'Lev Choubine',
+//           email: 'lev.choubine@gmail.com',
+//           password: '$2b$12$3HfeHDFs.MY1P1oBMwo30.jkW3ZB30.NZ.YTFYghgmO5Xrxj.geeC',
+//           createdAt: new Date(),
+//           updatedAt: new Date()
+//         },
   
      
-      ], { returning: true })
-    },
+//       ], { returning: true })
+//     },
   
-    down: function (queryInterface, Sequelize) {
-      return queryInterface.bulkDelete('users', null, {})
-    }
-  }
+//     down: function (queryInterface, Sequelize) {
+//       return queryInterface.bulkDelete('users', null, {})
+//     }
+//   }
